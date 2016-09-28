046ae1bc6f97c29d0c1945e83105aecec3314894
rohitksingh <r4rohit002@gmail.com>
2016-09-27 21:08:46 -0700

    Remote API design Updated Multiple slots added

--- src/CommandDesignPattern/Remote.java
+++ src/CommandDesignPattern/Remote.java
@@ -2,10 +2,10 @@ package CommandDesignPattern;
 
 public class Remote {
 
-	
+	/*
+	 * Old Version of Remote API
 	private Command command;;
-	
-	
+
 	public void setCoomand(Command command)
 	{
 		this.command=command;
@@ -15,6 +15,41 @@ public class Remote {
 	public void buttonPressed()
 	{
 		command.execute();
+	}
+	
+	*/
+	
+	/*
+	   Remote API DESIGN UPDATED
+	   Now Remote has multiple slots each slot has two buttons on off
+	   New code here
+	 */
+	
+	private Command[] onCommand;
+	private Command[] offCommand;
+	
+	public Remote()
+	{
+		onCommand=new Command[7];
+		offCommand=new Command[7];
+	}
+	
+	
+	public void setCommand(int slot, Command oncommand, Command offcommand) 
+	{
+		onCommand[slot] = oncommand;
+		offCommand[slot] = offcommand;
+	}
+	
+	
+	public void onButtonClicked(int slot)
+	{
+		onCommand[slot].execute();
+	}
+	
+	public void offButtonClicked(int slot)
+	{
+		offCommand[slot].execute();
 	}
 	
 }
--- src/CommandDesignPattern/RemoteTest.java
+++ src/CommandDesignPattern/RemoteTest.java
@@ -6,33 +6,32 @@ public class RemoteTest {
 	{
 		Light tableLamp=new Light();                                  ///<------------Target Object
 		
-		
-		LightOnCommand lightOn=new LightOnCommand(tableLamp);                 //Create CommandObject   pass target inside Command
- 		LightOffCommand lightOff=new LightOffCommand(tableLamp);              //Create CoomandObject
+		LightOnCommand tableLigthOn=new LightOnCommand(tableLamp);                 //Create CommandObject   pass target inside Command
+ 		LightOffCommand tableLightOff=new LightOffCommand(tableLamp);              //Create CoomandObject
 	   
  		
+ 		
+ 		
  		Remote remote=new Remote();                                 ///<--------------Invoker                                        
-	    remote.setCoomand(lightOn);                                 // Pass Command to invoker
-	    remote.buttonPressed();                                     // Invoker invoke method;                           
-	    remote.setCoomand(lightOff);    
-	    remote.buttonPressed();
 	    
-	    /*
-	     New class is added as per the requirement os client 
-	     And we did not make any modification in Remote API 
-	     GOOOOOOOD PROGRAMMING PRACTICE 
-	     class is not modified but it is extended for new changes.
-	     The Whole point of creating command objects.
-	     */
+	    Door frontDoor=new Door();
 	    
-        Door frontDoor=new Door();  
-	    OpenDoor openDoor=new OpenDoor(frontDoor);
-	    remote.setCoomand(openDoor);
-	    remote.buttonPressed();
-	    CloseDoor closeDoor=new CloseDoor(frontDoor);
-	    remote.setCoomand(closeDoor);
-	    remote.buttonPressed();
+	    OpenDoor openFrontDoor=new OpenDoor(frontDoor);
+	    CloseDoor closeFrontDoor=new CloseDoor(frontDoor);
+	    
+	    
+	    remote.setCommand(0, tableLigthOn, tableLightOff);
+	    remote.setCommand(1, openFrontDoor,closeFrontDoor);
+	    
+	    remote.onButtonClicked(0);
+	    remote.onButtonClicked(1);
+	    remote.offButtonClicked(0);
+	    remote.offButtonClicked(1);
+	    
+	   
+	   
+	   
+	    
 	    
 	}
-	
 }
